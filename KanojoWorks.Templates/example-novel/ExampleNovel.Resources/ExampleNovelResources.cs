42342127038967f976336ab701afd1f3e4aa0d3c
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-01-27 00:51:04 +0100

    Temporarily remove templates and fix solution files

--- KanojoWorks.Templates/example-novel/ExampleNovel.Resources/ExampleNovelResources.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace ExampleNovel.Resources
-{
-    public static class ExampleNovelResources
-    {
-        public static Assembly ResourceAssembly => typeof(ExampleNovelResources).Assembly;
-    }
-}
