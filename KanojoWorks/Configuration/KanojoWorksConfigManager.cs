77062125270a50c5a94411b897bd65c0e545e5d9
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-01-27 17:34:49 +0100

    Implement ConfigManager for basic features

---
+++ KanojoWorks/Configuration/KanojoWorksConfigManager.cs
@@ -0,0 +1,54 @@
+using osu.Framework.Configuration;
+using osu.Framework.Platform;
+using osu.Framework.Testing;
+
+namespace KanojoWorks.Configuration
+{
+    [ExcludeFromDynamicCompile]
+    public class KanojoWorksConfigManager : IniConfigManager<KanojoWorksSetting>
+    {
+        public KanojoWorksConfigManager(Storage storage)
+         : base(storage)
+        {
+        }
+
+        protected override void InitialiseDefaults()
+        {
+            base.InitialiseDefaults();
+
+            // Text / Reading defaults
+            Set(KanojoWorksSetting.TextDisplaySpeed, 0.8, 0, 1, 0.01);
+            Set(KanojoWorksSetting.AutoPlaySpeed, 0.8, 0, 1, 0.01);
+
+            Set(KanojoWorksSetting.SkipUnreadText, false).BindValueChanged((v) =>
+            {
+                if (v.NewValue) Set(KanojoWorksSetting.SkipUnreadText, true);
+            });
+
+            Set(KanojoWorksSetting.MarkReadText, true).BindValueChanged((v) =>
+            {
+                if (!v.NewValue) Set(KanojoWorksSetting.MarkReadText, false);
+            });
+
+            // Audio volume defaults
+            Set(KanojoWorksSetting.MasterVolume, 0.8, 0, 1, 0.01);
+            Set(KanojoWorksSetting.MenuVolume, 1.0, 0, 1, 0.01);
+            Set(KanojoWorksSetting.NovelBGMVolume, 0.8, 0, 1, 0.01);
+            Set(KanojoWorksSetting.VoiceMasterVolume, 1, 0, 1, 0.01);
+            Set(KanojoWorksSetting.NovelSFXVolume, 0.95, 0, 1, 0.01);
+        }
+    }
+
+    public enum KanojoWorksSetting
+    {
+        TextDisplaySpeed,
+        AutoPlaySpeed,
+        SkipUnreadText,
+        MarkReadText,
+        MasterVolume,
+        MenuVolume,
+        NovelBGMVolume,
+        VoiceMasterVolume,
+        NovelSFXVolume
+    }
+}
