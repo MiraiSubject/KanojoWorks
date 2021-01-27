d9614557d628ca09058f20350bb6c00b07d22d4f
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-01-27 19:21:04 +0100

    Remove unused private class in ChoiceContainer

--- KanojoWorks/Graphics/Novel/Containers/ChoiceContainer.cs
+++ KanojoWorks/Graphics/Novel/Containers/ChoiceContainer.cs
@@ -43,18 +43,5 @@ namespace KanojoWorks.Novel.Containers
         public void OnReleased(InputAction action)
         {
         }
-
-        private class ChoiceButton : KanojoWorksButton
-        {
-            // From: https://github.com/ppy/osu/blob/97879c3c98bcc19012c52e86287b2b8c8f0357ab/osu.Game/Screens/Play/GameplayMenuOverlay.cs#L298
-            // required to ensure keyboard navigation always starts from an extremity (unless the cursor is moved)
-            protected override bool OnHover(HoverEvent e) => true;
-
-            protected override bool OnMouseMove(MouseMoveEvent e)
-            {
-                Selected.Value = true;
-                return base.OnMouseMove(e);
-            }
-        }
     }
 }
