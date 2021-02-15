0524584310fec4ecf321bf80fb539d48f1ca2518
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-02-15 16:32:09 +0100

    Implement storage and configmanager.
    Also adds a note about the container resizing, considering its implementation is incomplete

--- KanojoWorks/KanojoWorksGameBase.cs
+++ KanojoWorks/KanojoWorksGameBase.cs
@@ -1,6 +1,6 @@
 
-using System.Collections.Generic;
 using System.Drawing;
+using KanojoWorks.Configuration;
 using osu.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
@@ -18,11 +18,12 @@ namespace KanojoWorks
     {
         private Bindable<Size> windowedResolution;
         private Bindable<WindowMode> windowMode;
-        private IEnumerable<Display> displays;
         private Bindable<Display> currentDisplay;
         private DependencyContainer dependencies;
         protected override Container<Drawable> Content { get; }
         protected Container<Drawable> nonScalingContent { get; private set; }
+        protected Storage Storage { get; set; }
+        protected KanojoWorksConfigManager ConfigManager;
 
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
             dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
@@ -35,6 +36,8 @@ namespace KanojoWorks
                 nonScalingContent = new Container
                 {
                     Size = new Vector2(1280, 720),
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
                 },
                 // Scaling content container that preserves drawsize for things like Menus, Overlays etc.
                 Content = new DrawSizePreservingFillContainer
@@ -53,18 +56,19 @@ namespace KanojoWorks
             windowMode = frameworkConfig.GetBindable<WindowMode>(FrameworkSetting.WindowMode);
             windowMode.ValueChanged += _ => updateContainerSize();
 
-            displays = Host.Window.Displays;
             currentDisplay = Host.Window.CurrentDisplayBindable;
 
             var builtInResources = new NamespacedResourceStore<byte[]>(new DllResourceStore(typeof(KanojoWorksGameBase).Assembly), @"Resources");
-           
             Resources.AddStore(builtInResources);
-            var Textures = new TextureStore(Host.CreateTextureLoaderStore(new NamespacedResourceStore<byte[]>(Resources, @"Textures")));
 
-            dependencies.Cache(Textures);
             dependencies.CacheAs(this);
+            Storage ??= Host.Storage;
+            dependencies.CacheAs(Storage);
+            dependencies.CacheAs(ConfigManager = new KanojoWorksConfigManager(Storage));
         }
 
+        // This currently resizes the nonscaling container regardless of aspect ratio,
+        // this should later become optional so users can choose between Maintain aspect ratio and no scaling at all.
         private void updateContainerSize()
         {
             if (windowMode.Value == WindowMode.Windowed)
