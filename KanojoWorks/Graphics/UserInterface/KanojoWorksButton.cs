ca65a2b4ccebe6e81406a6ed83ba15f7868eb016
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-02-25 16:00:56 +0100

    Also remove unnecessary remnants from an old project

--- KanojoWorks/Graphics/UserInterface/KanojoWorksButton.cs
+++ KanojoWorks/Graphics/UserInterface/KanojoWorksButton.cs
@@ -82,15 +82,9 @@ namespace KanojoWorks.Graphics.UserInterface
         private void selectionChanged(ValueChangedEvent<bool> value)
         {
             if (value.NewValue)
-            {
                 HoverArea.FadeIn(200, Easing.OutQuint);
-                //Content.TweenEdgeEffectTo(selectedTileEffect, 100, Easing.In);
-            }
             else
-            {
                 HoverArea.FadeOut(200, Easing.Out);
-                //Content.TweenEdgeEffectTo(unSelectedTileEffect, 100, Easing.Out);
-            }
         }
 
         protected override bool OnClick(ClickEvent e)
